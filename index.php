7017b1314784f84f8d2ad293911cac03621f55e8
nevh122 <69942560+nevh122@users.noreply.github.com>
2020-08-21 19:24:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
-<body>
-<center> <font size=6><h1 style="color:blue;">Hi! Im Nicolas Eduardo Huet</font></center></h1>
-<img src=https://trello-attachments.s3.amazonaws.com/5f3945025b439677dd72a20c/5f39befad087794a8819a945/a9b949337a9f522129f51c16ecdd78c6/117406283_962313754273372_3151311764514997581_n.jpg style="width:250px;height:350px;">
-<center><font size=4>Hello everyone I am Nicolas Eduardo Huet, but most people call me Ned as per my first names abbreviation.
+<body style="background-color:#de5246;">
+<center> <font size=7><h1 style="color:blue;">Hi! Im Nicolas Eduardo Huet</font></h1>
+<img src=https://trello-attachments.s3.amazonaws.com/5f3945025b439677dd72a20c/5f39befad087794a8819a945/a9b949337a9f522129f51c16ecdd78c6/117406283_962313754273372_3151311764514997581_n.jpg style="width:250px;height:350px;"><br>
+<font size=5 style="text-align:center">Hello everyone I am Nicolas Eduardo Huet, but most people call me Ned as per my first names abbreviation.
 
 I am a Introvert, meaning I will try to always be there to be a friend, classmate or someone to talk and not just a plain shut in. It just means that we introverts prefer to spend their free time much alone but doesn't mean we hate company. Video games are my go to games with focus on linear gameplay with hard stories. Same goes for books such as The Secrets of the Immortal Nicholas Fleming.
 
